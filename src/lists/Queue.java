3e62e99c6d94dd11a9760df293e3e72fc82c6744
Kevin Graham <kevin.edward.graham@gmail.com>
2018-03-02 17:29:18 +0700

    add isempty

--- src/lists/Queue.java
+++ src/lists/Queue.java
@@ -2,11 +2,16 @@ package lists;
 
 public class Queue<V> {
     private SingleLinkedList<V> list = new SingleLinkedList<>();
+    private boolean isEmpty;
 
+    public Queue() {
+        isEmpty = true;
+    }
     // adds item to stack
     public void enqueue(V item) {
         // insert item into front of list
         list.add(item);
+        isEmpty = false;
     }
 
     // removes and returns item from stack
@@ -14,12 +19,23 @@ public class Queue<V> {
         try {
             // get last item
             V item = list.get(0);
+
             // remove first item
             list.remove(0);
+
+            // check if empty
+            if (list.get(0) == null) {
+                isEmpty = true;
+            }
+
             // return first item
             return item;
         } catch (NullPointerException ex) {
             return null;
         }
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
     }
 }
